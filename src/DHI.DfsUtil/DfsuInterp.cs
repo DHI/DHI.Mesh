27ebb4ceef8aaa651d7241b33ff2f8906437bbab
Jesper Grooss <jgr@dhigroup.com>
2019-07-10 12:00:39 +0200

    Added DfsuUtil

---
+++ src/DHI.DfsUtil/DfsuInterp.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DHI.Generic.MikeZero;
+using DHI.Generic.MikeZero.DFS;
+using DHI.Generic.MikeZero.DFS.dfsu;
+using DHI.Mesh;
+
+namespace DHI.DfsUtil
+{
+  class DfsuInterp
+  {
+
+    public static readonly string InterpolateUsage =
+@"
+    -dfsuinterp: Interpolate dfsu file to another mesh:
+
+        DHI.DfsUtil -dfsuinterp [sourceFilename] [targetMeshFilename] [targetFilename]
+
+        Interpolate values from 'sourceFilename' to mesh defined by 
+        'targetMeshFilename', and store it in 'targetFilename'. 
+        The 'targetMeshFilename' can be a mesh or dfsu file.
+";
+
+    /// <summary>
+    /// Interpolate values from <paramref name="sourceFilename"/> to mesh
+    /// defined by <paramref name="targetMeshFilename"/>, and store it in
+    /// <paramref name="targetFilename"/>
+    /// </summary>
+    /// <param name="sourceFilename">Source data for interpolation</param>
+    /// <param name="targetMeshFilename">Target mesh to interpolate to. Can be a mesh or dfsu file</param>
+    /// <param name="targetFilename">File to store interpolated data to</param>
+    public static void Interpolate(string sourceFilename, string targetMeshFilename, string targetFilename)
+    {
+      System.Diagnostics.Stopwatch watch = new System.Diagnostics.Stopwatch();
+      watch.Start();
+
+      DfsFactory factory = new DfsFactory();
+      DfsuFile sourceDfsu = DfsFileFactory.DfsuFileOpen(sourceFilename);
+
+      DfsuBuilder builder = DfsuBuilder.Create(DfsuFileType.Dfsu2D);
+
+      DfsuBuildGeometry(targetMeshFilename, builder);
+      builder.SetTimeInfo(sourceDfsu.StartDateTime, sourceDfsu.TimeStepInSeconds);
+
+      // Add dynamic items, copying from source
+      foreach (DfsuDynamicItemInfo itemInfo in sourceDfsu.ItemInfo)
+      {
+        builder.AddDynamicItem(itemInfo.Name, itemInfo.Quantity);
+      }
+
+      DfsuFile targetDfsu = builder.CreateFile(targetFilename);
+
+      watch.Stop();
+      Console.Out.WriteLine("Create File : " + watch.Elapsed.TotalSeconds);
+      watch.Reset();
+      watch.Start();
+
+
+      MeshData sourceMesh = Create(sourceDfsu);
+      MeshData targetMesh = Create(targetDfsu);
+      sourceMesh.BuildDerivedData();
+
+
+      watch.Stop();
+      Console.Out.WriteLine("Build mesh  : " + watch.Elapsed.TotalSeconds);
+      watch.Reset();
+      watch.Start();
+
+      MeshInterpolator2D interpolator = new MeshInterpolator2D(sourceMesh)
+      {
+        DeleteValue = sourceDfsu.DeleteValueFloat,
+        DeleteValueFloat = sourceDfsu.DeleteValueFloat,
+      };
+      interpolator.SetTarget(targetMesh);
+
+      watch.Stop();
+      Console.Out.WriteLine("Interpolator: " + watch.Elapsed.TotalSeconds);
+      watch.Reset();
+      watch.Start();
+
+      // Temporary, interpolated target-data
+      float[] targetData = new float[targetDfsu.NumberOfElements];
+
+      // Add data for all item-timesteps, copying from source, interpolating
+      IDfsItemData<float> sourceData;
+      while (null != (sourceData = sourceDfsu.ReadItemTimeStepNext() as IDfsItemData<float>))
+      {
+        interpolator.InterpolateToTarget(sourceData.Data, targetData);
+        targetDfsu.WriteItemTimeStepNext(sourceData.Time, targetData);
+      }
+      watch.Stop();
+      Console.Out.WriteLine("Interpolate : " + watch.Elapsed.TotalSeconds);
+      watch.Reset();
+
+      sourceDfsu.Close();
+      targetDfsu.Close();
+    }
+
+    private static void DfsuBuildGeometry(string targetMeshFilename, DfsuBuilder builder)
+    {
+      DfsFactory factory = new DfsFactory();
+
+      if (targetMeshFilename.EndsWith(".mesh", StringComparison.OrdinalIgnoreCase))
+      {
+        MeshFile target = MeshFile.ReadMesh(targetMeshFilename);
+
+        // Setup header and geometry, copy from source file
+        builder.SetNodes(target.X, target.Y, target.Z.ToFloatArray(), target.Code);
+        builder.SetElements(target.ElementTable);
+        builder.SetProjection(factory.CreateProjection(target.Projection));
+        builder.SetZUnit(eumUnit.eumUmeter);
+      }
+      else
+      {
+        DfsuFile target = DfsFileFactory.DfsuFileOpen(targetMeshFilename);
+
+        // Setup header and geometry, copy from source file
+        builder.SetNodes(target.X, target.Y, target.Z, target.Code);
+        builder.SetElements(target.ElementTable);
+        builder.SetProjection(target.Projection);
+        builder.SetZUnit(eumUnit.eumUmeter);
+
+        target.Close();
+      }
+    }
+
+
+    public static MeshData Create(DfsuFile dfsu)
+    {
+      return MeshData.CreateMesh(dfsu.Projection.WKTString, dfsu.NodeIds, dfsu.X, dfsu.Y, dfsu.Z.ToDoubleArray(), dfsu.Code, dfsu.ElementIds, dfsu.ElementType, dfsu.ElementTable);
+    }
+    public static MeshData Create(MeshFile mesh)
+    {
+      return MeshData.CreateMesh(mesh.Projection, mesh.NodeIds, mesh.X, mesh.Y, mesh.Z, mesh.Code, mesh.ElementIds, mesh.ElementType, mesh.ElementTable);
+    }
+
+
+  }
+}
---
+++ src/DHI.DfsUtil/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DHI.DfsUtil
+{
+  class Program
+  {
+
+    /// <summary> Static constructor </summary>
+    static Program()
+    {
+      // The setup method will make your application find the MIKE assemblies at runtime.
+      // The first call of the setup method takes precedense. Any subsequent calls will be ignored.
+      // It must be called BEFORE any method using MIKE libraries is called, i.e. it is not sufficient
+      // to call it as the first thing in that method using the MIKE libraries. Often this can be achieved
+      // by having this code in the static constructor.
+      // If MIKE Core is x-copy deployed with the application, this is not required.
+      if (!DHI.Mike.Install.MikeImport.Setup(17, DHI.Mike.Install.MikeProducts.MikeCore))
+        throw new Exception("Cannot find a proper MIKE installation");
+    }
+
+    public static readonly string usage =
+@"
+Usage
+    DHI.DfsUtil -[tool] [arguments]
+
+Tools:
+"
+        + DfsuInterp.InterpolateUsage
+        + DfsDiff.CreateDiffFileUsage
+;
+
+    static void PrintUsage()
+    {
+      Console.Out.WriteLine(usage);
+    }
+
+
+    static void Main(string[] args)
+    {
+
+      if (args.Length == 0)
+      {
+        PrintUsage();
+        return;
+      }
+
+      string arg0 = args[0].ToLower();
+
+      // In honor of Microsoft Word and its super users, i.e. those using Word as their text-file-editor
+      // Word dislikes the '-' or '\u002D' hyphen, and is more than happy to replace it with '–' ('\u2013')
+      // '-' ('\u002D') is the standard hyphen character. These are other unicode hyphen characters
+      string hyphenChars = "\u05be\u1806\u2010\u2011\u2012\u2013\u2014\u2015\u2053\u2212\uFE58\uFE63\uFF0D";
+      // Check if first character is one of the special hyphen characters, replace with standard hyphen.
+      if (hyphenChars.IndexOf(arg0[0]) >= 0)
+        arg0 = "-" + arg0.Substring(1);
+
+      if (arg0.StartsWith("-dfsuinterp"))
+      {
+        if (args.Length != 4)
+        {
+          Console.Out.WriteLine("-dfsuinterp requires 3 arguments.");
+          PrintUsage();
+        }
+        DfsuInterp.Interpolate(args[1], args[2], args[3]);
+      }
+      else if (arg0.StartsWith("-diff"))
+      {
+        if (args.Length < 2)
+        {
+          Console.Out.WriteLine("-dfsuinterp requires 2 or 3 arguments.");
+          PrintUsage();
+        }
+
+        string file1 = args[1];
+        string file2 = args[2];
+        string filediff;
+        if (args.Length == 3)
+          //filediff = System.IO.Path.ChangeExtension(file1, ".diff" + System.IO.Path.GetExtension(file1));
+          filediff = null;
+        else
+          filediff = args[3];
+
+        DfsDiff.CreateDiffFile(file1, file2, filediff);
+
+      }
+      else
+      {
+        Console.Out.WriteLine("Tool "+arg0+" is not known.");
+        PrintUsage();
+      }
+    }
+  }
+}
