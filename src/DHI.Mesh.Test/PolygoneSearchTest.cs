53aa6a9047de4fd825539d99d1f61ead5a4e9806
Marc Ridler <44672709+marcridler@users.noreply.github.com>
2021-11-15 11:18:15 +0100

    The core method - search mesh for polygon.

---
+++ src/DHI.Mesh.Test/PolygoneSearchTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using DHI.Generic.MikeZero.DFS;
+using DHI.Generic.MikeZero.DFS.dfsu;
+using GeoAPI.Geometries;
+using NetTopologySuite.Geometries;
+using NUnit.Framework;
+
+namespace DHI.Mesh.Test
+{
+    internal class PolygoneSearchTest
+    {
+        [Test]
+        public void GetElementsIndicesWithWeight_PolygonWithinOneMeshElement()
+        {
+            var dfsufilepath = Path.GetFullPath(Path.Combine(@"..\..\..\..\..\", @"TestData\TestingDfsu1.dfsu"));
+            var mesh = GetMesh(dfsufilepath);
+            var gf = new GeometryFactory();
+
+            var coordinates = new Coordinate();
+
+            var polygon1 = gf.CreatePolygon(new Coordinate[] { new Coordinate(-0.1, 0.1), new Coordinate(-0.05, 0.1) , new Coordinate(-0.1, 0.15) });
+            var polygon2 = gf.CreatePolygon(new Coordinate[] { new Coordinate(0.05, 0.1), new Coordinate(0.1, 0.1), new Coordinate(0.07, 0.15) });
+
+            var searcher = new MeshSearcher(mesh);
+
+            var search1 = searcher.FindElementsAndWeight(polygon1);
+            var search2 = searcher.FindElementsAndWeight(polygon2);
+
+
+            Assert.Equals(1, search1.Count());
+
+            Assert.Equals(0, search1[0].Item1.Id);
+            Assert.Equals(1.0, search1[0].Item2);
+
+            Assert.Equals(13, search2[0].Item1.Id);
+            Assert.Equals(1.0, search2[0].Item2);
+        }
+
+
+        private MeshData GetMesh(string dfsufilepath)
+        {
+            MeshData mesh;
+
+            DfsuFile file = DfsFileFactory.DfsuFileOpen(dfsufilepath);
+            mesh = MeshData.CreateMesh(file.Projection.WKTString, file.NodeIds, file.X, file.Y, file.Z.ToDoubleArray(), file.Code, file.ElementIds, file.ElementType, file.ElementTable.ToZeroBased());
+            return mesh;
+        }
+    }
+}
--- src/DHI.Mesh/MeshSearcher.cs
+++ src/DHI.Mesh/MeshSearcher.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using GeoAPI.Geometries;
+using NetTopologySuite.Geometries;
 #if NTS173
 using GisSharpBlog.NetTopologySuite.Geometries;
 using GisSharpBlog.NetTopologySuite.Index.Strtree;
@@ -91,5 +93,69 @@ namespace DHI.Mesh
 
       return null;
     }
-  }
+
+        /// <summary>
+        /// Find elements either contained, containing or intersecting polygon. The Weight is equal to the proportinal 
+        /// area each element intersects with the polygon.
+        /// <para>
+        /// If polygon is totally contained within one mesh element, then 1 element is returned with weight 1.
+        /// If polygon partially falls outside of the grid, only elements within grid are returned.
+        /// </para>
+        /// </summary>
+        public IList<(MeshElement,double)> FindElementsAndWeight(IPolygon polygon)
+        {
+            // Find potential elements for (x,y) point
+            Envelope targetEnvelope = polygon.Boundary.EnvelopeInternal;
+            GeometryFactory gm = new GeometryFactory();
+            IList<(MeshElement, double)> result = new List<(MeshElement, double)>();
+
+#if NTS173
+      IList potentialSourceElmts = _elementSearchTree.Query(targetEnvelope);
+#else
+            IList<MeshElement> potentialSourceElmts = _elementSearchTree.Query(targetEnvelope);
+#endif
+
+            // Loop over all potential elements
+            for (int i = 0; i < potentialSourceElmts.Count; i++)
+            {
+#if NTS173
+        MeshElement element = (MeshElement)potentialSourceElmts[i];
+#else
+                MeshElement element = potentialSourceElmts[i];
+#endif
+                Coordinate[] coordinates = element.Nodes
+                    .Select(node => new Coordinate(node.X, node.Y, node.Z))
+                    .ToArray();
+                IPolygon elementPolygon = gm.CreatePolygon(coordinates.ToArray());
+
+                if (elementPolygon.Contains(polygon))
+                {
+                    result.Add((element, 1.0));
+                    continue;
+                }
+
+                if (elementPolygon.Intersects(polygon))
+                {
+                    result.Add((element, elementPolygon.Intersection(polygon).Area));
+                    continue;
+                }
+
+                if (polygon.Contains(elementPolygon))
+                {
+                    result.Add((element, elementPolygon.Area));
+                }
+            }
+            if (result.Count == 0)
+            {
+                return null;
+            }
+
+            var area = result.Sum(e => e.Item2);
+            for (int i = 0; i < result.Count; i++)
+            {
+                result[i] = (result[i].Item1, result[i].Item2 / area);
+            }
+            return result.Count == 0 ? null : result;
+        }
+    }
 }
