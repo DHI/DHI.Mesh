0c74275fb61e6419c89bd90fdc59d03ee40714c6
Lars Michael <lars.michael.dk@gmail.com>
2021-12-08 19:40:16 +0100

    Also delete old symbol package for prereleases

--- src/DHI.Mesh/nuget/PackAndPush-prerelease.bat
+++ src/DHI.Mesh/nuget/PackAndPush-prerelease.bat
@@ -3,6 +3,7 @@ for /f "tokens=2 delims==" %%I in ('wmic os get localdatetime /format:list') do 
 set logfile=%datetime:~0,8%-%datetime:~8,6%.log
 
 del ..\bin\%configuration%\*.nupkg 2>nul
+del ..\bin\%configuration%\*.snupkg 2>nul
 dotnet pack ..\DHI.Mesh.csproj --configuration %configuration% > %logfile% 2>&1
 dotnet nuget push ..\bin\%configuration%\DHI.Mesh.*.nupkg --source http://dhi-nuget-server.azurewebsites.net/nuget --api-key dhi-nuget-admin >> %logfile% 2>&1
 
